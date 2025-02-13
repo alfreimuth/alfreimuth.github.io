fe2d98cf0fd8cafad370101b9a1d70b16f3f20f1
Freimuth <500211023@fvtc.edu>
2025-02-13 12:29:01 -0600

    Added Dynamic Welcome

--- site.js
+++ site.js
@@ -1,1 +1,27 @@
 console.log('hi')
+
+const hours = new Date().getHours()
+
+const isMorning = hours >= 4 && hours < 12
+const isAfternoon = hours >= 12 && hours < 17
+const isEvening = hours >= 17 || hours < 4
+
+let greet = ''
+
+if(isMorning) {
+    console.log('Morning')
+    greet = 'Morning'
+}
+else if(isAfternoon) {
+    console.log('Afternoon')
+    greet = 'Afternoon'
+}
+else {
+    console.log('Evening')
+    greet = 'Evening'
+}
+
+
+const welcome = document.getElementById("welcome")
+
+welcome.textContent = 'Good ' + greet + '!'
